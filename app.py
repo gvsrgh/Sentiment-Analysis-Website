26845f584a10e6ca183c8371132f535ae9738cfd
C-S-V-S-Subrahmanyam <151535563+C-S-V-S-Subrahmanyam@users.noreply.github.com>
2025-04-02 06:41:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,5 +87,5 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))  # Render assigns a port dynamically
+    app.run(host="0.0.0.0", port=port, debug=True)
